94b26be2e2d97b05df9da2ac4f37cf482416f801
Reiz <137693283+Reiz-0@users.noreply.github.com>
2023-11-10 14:38:43 +0800

    Create User.cs

---
+++ Zetavers/Game/User/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Zetavers.Game.User
+{
+    internal partial class User
+    {
+        public static string PlayerName = Environment.UserName;
+        public static double Spart = 0.00;
+        public static int Fragments = 0;
+
+        public static double XP = 0;
+        public static int Level = 1;
+    }
+}
